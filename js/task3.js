1202fb3b2b62375405f20570d8dfd05ede652a3e
MironenkoNadiia <candleflame@i.ua>
2020-09-21 23:09:51 +0300

    task4

--- js/task3.js
+++ js/task3.js
@@ -1,5 +1,6 @@
 // Write code under this line
-const getUsersWithGender = (array, gender) => array.filter(el => el.gender === gender).map(({ name }) => name);
+const getUsersWithGender = (array, gender) => array.filter(el =>
+    el.gender === gender).map(({ name }) => name);
 
 console.log(getUsersWithGender(users, 'male'));
 
